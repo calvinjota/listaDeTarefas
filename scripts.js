39c77ada645312edbc2a45a84eb4d4c929293b2a
Calvin Jota <72287665+calvinjota@users.noreply.github.com>
2022-04-21 08:10:53 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,28 @@ const inputElement = document.querySelector(".new_task_input");
 const addTaskButton = document.querySelector(".new_task_button");
 const tasksContainer = document.querySelector(".task_container");
 
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+const clock = setInterval(function time() {
+    let dateToday = new Date();
+
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let sec = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+
+    if (min < 10) min = '0' + min; 
+
+    if (sec < 10) sec = '0' + sec;
+
+    hours.textContent = hr;
+    minutes.textContent = min;
+    seconds.textContent = sec;
+})
+
 const validateInput = () => inputElement.value.trim().length > 0;
 
 const handleAddTask = () => {
